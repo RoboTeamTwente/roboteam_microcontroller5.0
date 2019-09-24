2ab8745bc545a0cdd9d7270b96df2075d533e61b
Jordi <jordiluong@gmail.com>
2019-09-24 10:31:05 +0200

    Changed transformations to use defined frame

--- Core/Src/stateEstimation.c
+++ Core/Src/stateEstimation.c
@@ -26,6 +26,13 @@ void stateEstimation_Update(StateInfo* input) {
 	float vel[2] = {0.0f};
 	wheels2Body(input->wheelSpeeds, vel);
 
+	static int count = 0;
+	if (count > 10) {
+		Putty_printf("%f %f \n\r", vel[0], vel[1]);
+		count = 0;
+	}
+	count++;
+
 	kalman_CalculateK();
 	kalman_Update(input->xsensAcc, vel);
 
@@ -51,7 +58,7 @@ static void wheels2Body(float wheelSpeeds[4], float output[3]){
 	static const float wheels2BodyJ = cosFront / (2 * pow(cosFront, 2) + 2 * pow(cosBack, 2));
 	static const float wheels2BodyK = sinBack / (2 * sinFront + 2 * sinBack);
 
-	output[body_x] = wheels2BodyI * (-wheelSpeeds[wheels_RF] - wheelSpeeds[wheels_LF] + wheelSpeeds[wheels_LB] + wheelSpeeds[wheels_RB]) * rad_wheel;
-	output[body_y] = (wheels2BodyJ * (wheelSpeeds[wheels_RF] - wheelSpeeds[wheels_LF]) + (1 - wheels2BodyJ) * (-wheelSpeeds[wheels_LB] + wheelSpeeds[wheels_RB])) * rad_wheel;
+	output[body_y] = wheels2BodyI * (wheelSpeeds[wheels_RF] + wheelSpeeds[wheels_LF] - wheelSpeeds[wheels_LB] - wheelSpeeds[wheels_RB]) * rad_wheel;
+	output[body_x] = (wheels2BodyJ * (wheelSpeeds[wheels_RF] - wheelSpeeds[wheels_LF]) + (1 - wheels2BodyJ) * (-wheelSpeeds[wheels_LB] + wheelSpeeds[wheels_RB])) * rad_wheel;
 	output[body_w] = (wheels2BodyK * (wheelSpeeds[wheels_RF] + wheelSpeeds[wheels_LF]) + (1 - wheels2BodyK) * (wheelSpeeds[wheels_LB] + wheelSpeeds[wheels_RB])) / rad_robot * rad_wheel;
 }
