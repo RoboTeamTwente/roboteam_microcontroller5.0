6b0da15ce16fc945ce07f55d0effaa3647cd2aae
Jordi <jordiluong@gmail.com>
2020-06-19 13:10:27 +0200

    tune

--- Util/control_util.h
+++ Util/control_util.h
@@ -69,7 +69,7 @@ float WHEEL_REF_LIMIT; // [rad/s] Limit the maximum wheel reference to leave roo
 
 // Shoot
 #define MIN_KICK_TIME 8.2 				// minimum time period of kicking
-#define MAX_KICK_TIME 45 				// maximum time period of kicking
+#define MAX_KICK_TIME 120 				// maximum time period of kicking
 #define MIN_CHIP_TIME 60 				// minimum time period of chipping
 #define MAX_CHIP_TIME 160 				// maximum time period of chipping
 #define TIMER_FREQ 10000 			// frequency [Hz] of TIM6  (Clock frequency divided by prescaler)
