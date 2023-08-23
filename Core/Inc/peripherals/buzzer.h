f01ab1b70634251e2db4f19e079199bc7b20922b
Tom Meulenkamp <t.meulenkamp@roboteamtwente.nl>
2023-08-23 18:21:04 +0200

    Refactor battery drain noise

--- Core/Inc/peripherals/buzzer.h
+++ Core/Inc/peripherals/buzzer.h
@@ -31,7 +31,7 @@ void buzzer_Play_Startup();
 void buzzer_Play_Tetris();
 void buzzer_Play_Mario();
 void buzzer_Play_PinkPanther();
-void buzzer_Play_PowerUp();
+void buzzer_Play_BatteryDrainWarning();
 void buzzer_Play_WarningOne();
 void buzzer_Play_WarningTwo();
 void buzzer_Play_WarningThree();
