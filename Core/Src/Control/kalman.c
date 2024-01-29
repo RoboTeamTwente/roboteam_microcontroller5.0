50bf84d98b07ad8ef4e27ceb9d3667544d1f3feb
Julie <j.muchembled@roboteamtwente.nl>
2024-01-29 10:02:06 +0100

    Removing the if loop for calculating Kalman gain (K) when count < 100

--- Core/Src/Control/kalman.c
+++ Core/Src/Control/kalman.c
@@ -32,97 +32,96 @@ void kalman_Update(float acc[2], float vel[2]){
 	//	for (int i = 0; i < STATE; i++) {
 	//		aU[i] = controlInput[i];
 	//	}
-		float correct_acc[2] = {0.0f, 0.0f}; // to not overwrite raw acceleration (acc[2])
-		correct_acc[0] = -1 * acc[0]; // xsensAcc1 is in opposite direction of robot velocity v
-		correct_acc[1] = acc[1];  
+	float correct_acc[2] = {0.0f, 0.0f}; // to not overwrite raw acceleration (acc[2])
+	correct_acc[0] = -1 * acc[0]; // xsensAcc1 is in opposite direction of robot velocity v
+	correct_acc[1] = acc[1];  
 
-		float smoothed_Acc[2] = {0.0f, 0.0f};
-		smoothen_xsensAcc(correct_acc, smoothed_Acc);
-		az[0] = vel[0];
-		az[1] = smoothed_Acc[1]; // xsensAcc2 = same axis as robot velocity u
-		az[2] = vel[1];
-		az[3] = smoothed_Acc[0]; // xsensAcc1 = same axis as robot velocity v
+	float smoothed_Acc[2] = {0.0f, 0.0f};
+	smoothen_xsensAcc(correct_acc, smoothed_Acc);
+	az[0] = vel[0];
+	az[1] = smoothed_Acc[1]; // xsensAcc2 = same axis as robot velocity u
+	az[2] = vel[1];
+	az[3] = smoothed_Acc[0]; // xsensAcc1 = same axis as robot velocity v
 
-		// Computes the formula:
-		// Xk = Fk * X(k-1) + Bk * Uk
-		multiplyMatrix(aF, aXold, aFX, STATE, 1, STATE);
-		multiplyMatrix(aB, aU, aBU, STATE, 1, STATE);
-		addMatrix(aFX, aBU, aXcurrent, STATE / 2, STATE / 2);
+	// Computes the formula:
+	// Xk = Fk * X(k-1) + Bk * Uk
+	multiplyMatrix(aF, aXold, aFX, STATE, 1, STATE);
+	multiplyMatrix(aB, aU, aBU, STATE, 1, STATE);
+	addMatrix(aFX, aBU, aXcurrent, STATE / 2, STATE / 2);
 
-		// Process data
-		// Yk = Zk - Hk * Xk
-		multiplyMatrix(aH, aXcurrent, aHX, OBSERVE, 1, STATE);
-		subMatrix(az, aHX, ayold, OBSERVE / 2, OBSERVE / 2);
+	// Process data
+	// Yk = Zk - Hk * Xk
+	multiplyMatrix(aH, aXcurrent, aHX, OBSERVE, 1, STATE);
+	subMatrix(az, aHX, ayold, OBSERVE / 2, OBSERVE / 2);
 
-		// Update
-		// X(k+1) = Kk * Yk + Xk
-		multiplyMatrix(aK, ayold, aKy, STATE, 1, OBSERVE);
-		addMatrix(aXcurrent, aKy, aXnew, STATE / 2, STATE / 2);
+	// Update
+	// X(k+1) = Kk * Yk + Xk
+	multiplyMatrix(aK, ayold, aKy, STATE, 1, OBSERVE);
+	addMatrix(aXcurrent, aKy, aXnew, STATE / 2, STATE / 2);
 
-		for (int i=0; i<STATE; i++){
-			aXold[i] = aXnew[i];
-		}
+	for (int i=0; i<STATE; i++){
+		aXold[i] = aXnew[i];
+	}
 
 }
 
 void kalman_CalculateK(){
 
 	static float count = 0;
-	if (count < 100){
-		static float oldk[STATE*OBSERVE] = {0};
 
-		// Calculates the predicted estimate covariance
-		// Pk = Fk * P(k-1) * Ftk + Qk
-		multiplyMatrix(aF, aPold, aFP, STATE, STATE, STATE);
-		multiplyMatrix(aFP, aFt, aFPFt, STATE, STATE, STATE);
-		addMatrix(aFPFt, aQ, aPcurrent, STATE, STATE);
+	static float oldk[STATE*OBSERVE] = {0};
 
-		// Calculates the innovation covariance
-		// Sk = Pk * Htk * Hk + Rk
-		multiplyMatrix(aPcurrent, aHt, aPHt, STATE, OBSERVE, STATE);
-		multiplyMatrix(aH, aPHt, aHPHt, OBSERVE, OBSERVE, STATE);
-		addMatrix(aR, aHPHt, aS, OBSERVE, OBSERVE);
+	// Calculates the predicted estimate covariance
+	// Pk = Fk * P(k-1) * Ftk + Qk
+	multiplyMatrix(aF, aPold, aFP, STATE, STATE, STATE);
+	multiplyMatrix(aFP, aFt, aFPFt, STATE, STATE, STATE);
+	addMatrix(aFPFt, aQ, aPcurrent, STATE, STATE);
 
-		// Compute Kalman Gain
-		// Kk = Pk * Htk * Sik
-		inverse(aS, aSi, OBSERVE);
-		multiplyMatrix(aPHt, aSi, aK, STATE, OBSERVE, OBSERVE);
+	// Calculates the innovation covariance
+	// Sk = Pk * Htk * Hk + Rk
+	multiplyMatrix(aPcurrent, aHt, aPHt, STATE, OBSERVE, STATE);
+	multiplyMatrix(aH, aPHt, aHPHt, OBSERVE, OBSERVE, STATE);
+	addMatrix(aR, aHPHt, aS, OBSERVE, OBSERVE);
 
-		// Calculate the posteriori estimate covariance matrix
-		// Pk = (I - Kk * Hk) * P(k - 1) * (I - Kk * Hk)t + Kk * Rk * Ktk
+	// Compute Kalman Gain
+	// Kk = Pk * Htk * Sik
+	inverse(aS, aSi, OBSERVE);
+	multiplyMatrix(aPHt, aSi, aK, STATE, OBSERVE, OBSERVE);
 
-		// KRKt = Kk * Rk * Ktk
-		transMatrix(aK, aKt, STATE, OBSERVE);
-		multiplyMatrix(aK, aR, aKR, STATE, OBSERVE, OBSERVE);
-		multiplyMatrix(aKR, aKt, aKRKt, STATE, STATE, OBSERVE);
+	// Calculate the posteriori estimate covariance matrix
+	// Pk = (I - Kk * Hk) * P(k - 1) * (I - Kk * Hk)t + Kk * Rk * Ktk
 
-		// I-KH = I - Kk * Hh
-		multiplyMatrix(aK, aH, aKH, STATE, STATE, OBSERVE);
-		subMatrix(aI, aKH, aI_KH, STATE, STATE);
+	// KRKt = Kk * Rk * Ktk
+	transMatrix(aK, aKt, STATE, OBSERVE);
+	multiplyMatrix(aK, aR, aKR, STATE, OBSERVE, OBSERVE);
+	multiplyMatrix(aKR, aKt, aKRKt, STATE, STATE, OBSERVE);
 
-		// (I-KH)t
-		transMatrix(aI_KH, aI_KHt, STATE, STATE);
-		
-		multiplyMatrix(aI_KH, aPcurrent, aI_KHP, STATE, STATE, STATE);
-		multiplyMatrix(aI_KHP, aI_KHt, aI_KHPI_KHt, STATE, STATE, STATE);
-		addMatrix(aI_KHPI_KHt, aKRKt, aPnew, STATE, STATE);
+	// I-KH = I - Kk * Hh
+	multiplyMatrix(aK, aH, aKH, STATE, STATE, OBSERVE);
+	subMatrix(aI, aKH, aI_KH, STATE, STATE);
+
+	// (I-KH)t
+	transMatrix(aI_KH, aI_KHt, STATE, STATE);
+	
+	multiplyMatrix(aI_KH, aPcurrent, aI_KHP, STATE, STATE, STATE);
+	multiplyMatrix(aI_KHP, aI_KHt, aI_KHPI_KHt, STATE, STATE, STATE);
+	addMatrix(aI_KHPI_KHt, aKRKt, aPnew, STATE, STATE);
 
 
-		float same = 0;
-		for (int i=0; i<STATE*OBSERVE; i++){
-			if (oldk[i] - aK[i] < 0.0000001 && oldk[i] - aK[i] > -0.0000001){
-				same +=1;
-			}
-			oldk[i] = aK[i];
+	float same = 0;
+	for (int i=0; i<STATE*OBSERVE; i++){
+		if (oldk[i] - aK[i] < 0.0000001 && oldk[i] - aK[i] > -0.0000001){
+			same +=1;
 		}
+		oldk[i] = aK[i];
+	}
 
-		if (same == STATE*OBSERVE){
-			count += 1;
-		}
+	if (same == STATE*OBSERVE){
+		count += 1;
+	}
 
-		for (int i=0; i<STATE*STATE; i++){
-			aPold[i] = aPnew[i];
-		}
+	for (int i=0; i<STATE*STATE; i++){
+		aPold[i] = aPnew[i];
 	}
 }
 
@@ -145,6 +144,7 @@ void kalman_GetP(float P[STATE*STATE]) {
 	for (int i = 0; i < STATE*STATE; i++) {
 		P[i] = aPold[i];
 	}
+}
 
 ///////////////////////////////////////////////////// PRIVATE FUNCTION IMPLEMENTATIONS
 
